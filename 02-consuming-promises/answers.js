518cd2feb5f108fb707b93e3f10eab7488a67c6e
tamartzuf <ttzuf1804@gmail.com>
2021-10-13 17:40:31 +0300

    02-consuming-promises

--- 02-consuming-promises/answers.js
+++ 02-consuming-promises/answers.js
@@ -7,7 +7,8 @@
  * 
  */
 function waitForPromise(promise, action){
-  /* IMPLEMENT ME */
+  promise
+  .then(action);
 }
 /**
  * 
@@ -18,7 +19,10 @@ function waitForPromise(promise, action){
  * @param {handler} handler 
  */
 function consumePromise(promise, consumer, handler){
-  /* IMPLEMENT ME! */
+  promise
+  .then(consumer)
+  .catch(handler);
+  
 }
 
 /**
